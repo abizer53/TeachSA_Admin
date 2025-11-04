aab202e83c9ec27f74e31c5e374e3c806d8f15a6
kanwaljeet5124 <kanwaljeetsingh5124@gmail.com>
2025-11-04 18:09:50 +0530

    report section updation

--- src/modules/SingleReport.js
+++ src/modules/SingleReport.js
@@ -1,13 +1,14 @@
 import dayjs from 'dayjs'
+import Link from 'next/link'
 import React from 'react'
 
 export default function SingleReport({data}) {
   return (
     <div className='w-full bg-white border-b border-border-nput flex items-center justify-between px-5 py-4'>
         <h6 className='text-sm font-normal text-black w-1/6'>{data?.type == 'JobSeekerReport' ? 'Seeker':'Employer'}</h6>
-        <h6 className='text-sm font-normal text-black w-1/6 capitalize'>{data?.employer_name || 'N/A'}</h6>
-        <h6 className='text-sm font-normal text-black w-1/6 capitalize'>{data?.seeker_name || 'N/A'}</h6>
-        <h6 className='text-sm font-normal text-black w-1/6 capitalize'>{dayjs(data?.created_at).format("DD MMM, YYYY HH:mm A")}</h6>
+        <Link href={`/job-providers/${data?.Employer_id}`} target='_blank' rel='noreferrer' className='text-sm font-normal text-black w-1/6 capitalize'>{data?.employer_name || 'N/A'}</Link>
+        <Link href={`/job-seekers/${data?.JobSeeker_id}`} target='_blank' rel='noreferrer' className='text-sm font-normal text-black w-1/6 capitalize'>{data?.seeker_name || 'N/A'}</Link>
+        <h6 className='text-sm font-normal text-black w-1/6 capitalize'>{dayjs(data?.created_at).format("DD MMM, YYYY")}</h6>
         <h6 className='text-sm font-normal text-black w-2/6'>{data?.message || 'Not coming'}</h6>
     </div>
   )
